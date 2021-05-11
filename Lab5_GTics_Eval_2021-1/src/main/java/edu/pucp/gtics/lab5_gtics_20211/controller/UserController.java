056a26161dfbbdbdd1063e75115f9d1cde0701d6
JohamRV <a20181459@pucp.edu.pe>
2021-05-11 12:42:51 -0500

    a

--- Lab5_GTics_Eval_2021-1/src/main/java/edu/pucp/gtics/lab5_gtics_20211/controller/UserController.java
+++ Lab5_GTics_Eval_2021-1/src/main/java/edu/pucp/gtics/lab5_gtics_20211/controller/UserController.java
@@ -32,7 +32,7 @@ public class UserController {
 
     @GetMapping("/user/signInRedirect")
     public String signInRedirect(Authentication auth, HttpSession session){
-       return "/juegos/lista";
+       return "redirect:/juegos/lista";
 
     }
 
